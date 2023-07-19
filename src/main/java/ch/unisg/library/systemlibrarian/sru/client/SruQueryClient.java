76d105f875c5288daa8e608492ebc9d8a46adf17
Jonas Furrer <jonas.furrer@gmail.com>
2023-07-19 09:14:05 +0200

    Add warnings regarding max records

--- src/main/java/ch/unisg/library/systemlibrarian/sru/client/SruQueryClient.java
+++ src/main/java/ch/unisg/library/systemlibrarian/sru/client/SruQueryClient.java
@@ -4,9 +4,12 @@ import ch.unisg.library.systemlibrarian.helper.XPathHelper;
 import ch.unisg.library.systemlibrarian.sru.query.SruQuery;
 import ch.unisg.library.systemlibrarian.sru.response.MarcRecord;
 import io.mikael.urlbuilder.UrlBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
+import java.lang.invoke.MethodHandles;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
@@ -14,19 +17,19 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class SruQueryClient {
+	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	static final String QUERY = "query";
 	static final String START_RECORD = "startRecord";
 	static final int DEFAULT_START_RECORD = 1;
 	static final String MAXIMUM_RECORDS = "maximumRecords";
-	static final int DEFAULT_MAXIMUM_RECORDS = 10;
+	static final int DEFAULT_MAXIMUM_RECORDS = 50;
 	private final String baseUrl;
 	private final Version version;
 	private final SruQuery query;
 	private int startRecord = DEFAULT_START_RECORD;
 	private int maximumRecords = DEFAULT_MAXIMUM_RECORDS;
 	private RecordSchema recordSchema = RecordSchema.MARCXML;
-
 
 	SruQueryClient(
 			final String baseUrl,
@@ -43,6 +46,9 @@ public class SruQueryClient {
 	}
 
 	public SruQueryClient maximumRecords(final int maximumRecords) {
+		if (maximumRecords > 50) {
+			LOG.warn("maximumRecords must be between 1 and 50. Falling back to 50.");
+		}
 		this.maximumRecords = maximumRecords;
 		return this;
 	}
@@ -62,6 +68,7 @@ public class SruQueryClient {
 	 * @return Optional record, depending on the query, the only result or the first of the result
 	 */
 	public Optional<MarcRecord> getSingleRecord() {
+		LOG.warn("Retrieving only one record, ignoring maximumRecords");
 		URI callUri = UrlBuilder.fromUri(getSruUri())
 				.setParameter(MAXIMUM_RECORDS, String.valueOf(1))
 				.toUri();
@@ -89,9 +96,9 @@ public class SruQueryClient {
 	 * @return All resulting records without paging
 	 */
 	public Stream<MarcRecord> getAllRecords() {
-		int pageSize = 50;
-		return IntStream.iterate(1, i -> i + pageSize)  // Creates an infinite Stream with elements 0, 50, 100, 150, ...
-				.mapToObj(offset -> call(getSruUri(), offset, pageSize))
+		LOG.warn("Retrieving all available records, ignoring maximumRecords");
+		return IntStream.iterate(1, i -> i + DEFAULT_MAXIMUM_RECORDS)  // Creates an infinite Stream with elements 0, 50, 100, 150, ...
+				.mapToObj(offset -> call(getSruUri(), offset, DEFAULT_MAXIMUM_RECORDS))
 				.takeWhile(result -> result.isPresent() && hasRecords(result.get()))
 				.flatMap(Optional::stream)
 				.flatMap(this::extractRecords);
